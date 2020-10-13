e1368e0ca7a40168e99ca0d464d60c503b2e6efa
loverofMush <tolujamal1@gmail.com>
2020-10-13 19:04:58 +0100

    - created permissions helper

---
+++ app/perm_helpers.py
@@ -0,0 +1,78 @@
+from app.models import permission, role
+from app.db import session
+
+Permission = permission.Permission
+
+class HasPermissionTrait:
+    """ Assign permissions to a user
+        @return permissions or null
+    """
+    def givePermissionsTo():
+        permissions = self.getAllPermissions()
+        
+        if permissions is None:
+            return self
+        
+        permission = Permission(permissions)
+        session.add_all(permission)
+        session.commit()
+        return permission
+        
+        
+    """
+    Revoke a specified permission from a user.
+    Permissions are only revoked if they are in the scope any of the user's
+    roles. If the permission is out of scope, a RolePermissionScopeException
+    is raised.
+    """
+    def revokePermissions(*perms):
+        permissions = self.getAllPermissions()
+        
+        permission = Permission(permissions)
+        session.delete(permission)
+        session.commit()
+        return self
+    
+    
+    """Check if a user has permission to perform an action."""
+    def hasPermissionTo(perm):
+        return self.hasPermissionThroughRole(perm) or self.hasPermission(perm)
+        
+        
+    """hasPermission through role"""
+    def hasPermissionThroughRole(perm):
+        if type(perm) == str:
+            permission = Permission.query.filter_by(name=perm.name).first()
+            role = Permission.roles.first()
+            roles = self.query.all()
+            
+            if roles.contains(role):
+                return True
+        else:
+            for role in perm.roles:
+                if roles.contains(role):
+                    return True
+        
+        return False
+    
+    
+    """check if user has required role"""
+    def hasRole(*roles):
+        user_roles = self.query.all()
+        for role in roles:
+            if user_roles.contains(role):
+                return True
+            return False
+        
+        
+    """get all permissions"""
+    def getAllPermissions():
+        return Permission.query.all()
+    
+    """check if user has direct permissions"""
+    def hasPermission(perm):
+        permit = Permission.query.filter_by(slug = perm.slug).first()
+        if permit is None:
+            return False
+            raise('User does not have permission')
+        return True
