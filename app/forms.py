e4fed29de4ecb648e98891c37bc1a92ae7f9c829
loverofMush <tolujamal1@gmail.com>
2020-10-13 14:36:17 +0100

    - forms module

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,67 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField, SelectField
+from wtforms import StringField, TextField, SubmitField, SelectField, PasswordField, RadioField
 from wtforms.validators import DataRequired, Length
+
+"""Create User Form"""
+class CreateUserForm(FlaskForm):
+    selection_field = SelectField()
+    def __init__(self):
+        super(CreateUserForm, self).__init__()
+        
+        from app.models.roles import Roles
+        self.selection_field.choices = Roles.query.all()
+        
+    username = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=6, message='Password too short')
+        ]
+    )
+    active = RadioField(
+        'Active User',
+        validators=[DataRequired()]
+    )
+    role = SelectField(
+        'User Role',
+        validators=[DataRequired()],
+        coerce=int
+    )
+    submit = SubmitField('Create User')
+    
+"""Login Form"""
+class LoginForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators = [
+            DataRequired(),
+            Length(min=6, message='Enter a valid username')
+        ]
+    )
+    password = PasswordField(
+        'password',
+        validators = [DataRequired()]
+    )
+    submit = SubmitField('Log In')
+    
+    
+"""Role Creation Form"""
+class RoleForm(FlaskForm):
+    title = StringField(
+        'Role',
+        validators = [DataRequired()]
+    )
+    submit = SubmitField('Add Role')
+    
+"""Permission Creation Form"""
+class PermissionForm(FlaskForm):
+    name = StringField(
+        'Permission',
+        validators = [DataRequired()]
+    )
+    submit = SubmitField('Add Permission')
+    
