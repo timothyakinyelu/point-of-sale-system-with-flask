c7649bb093289a29ed6a9900eca7356a44cbda8f
loverofMush <tolujamal1@gmail.com>
2020-10-16 19:47:45 +0100

    - function comments

--- app/perm_helpers.py
+++ app/perm_helpers.py
@@ -4,11 +4,12 @@ from app.db import session
 Permission = permission.Permission
 
 class HasPermissionTrait:
-    """ Assign permissions to a user or role
-        @return permissions or null
-    """
     
     def givePermissionsTo(self, perms):
+        """ Assign permissions to a user or role
+            @return permissions or null
+        """
+        
         # permissions = self.getAllPermissions(perms) 
         if perms is None:
             return self
@@ -19,13 +20,14 @@ class HasPermissionTrait:
         return self
         
         
-    """
-    Revoke a specified permission from a user.
-    Permissions are only revoked if they are in the scope any of the user's
-    roles. If the permission is out of scope, a RolePermissionScopeException
-    is raised.
-    """
     def revokePermissions(self, perms):
+        """
+            Revoke a specified permission from a user.
+            Permissions are only revoked if they are in the scope any of the user's
+            roles. If the permission is out of scope, a RolePermissionScopeException
+            is raised.
+        """
+        
         if perms is not None:
             for perm in perms: 
                 self.permissions.remove(perm)
@@ -34,13 +36,15 @@ class HasPermissionTrait:
             return self
     
     
-    """Check if a user has permission to perform an action."""
     def hasPermissionTo(perm):
+        """Check if a user has permission to perform an action."""
+        
         return self.hasPermissionThroughRole(perm) or self.hasPermission(perm)
         
         
-    """hasPermission through role"""
     def hasPermissionThroughRole(perm):
+        """hasPermission through role"""
+        
         if type(perm) == str:
             permission = Permission.query.filter_by(name=perm.name).first()
             role = Permission.roles.first()
@@ -56,8 +60,9 @@ class HasPermissionTrait:
         return False
     
     
-    """check if user has required role"""
     def hasRole(*roles):
+        """check if user has required role"""
+        
         user_roles = self.query.all()
         for role in roles:
             if user_roles.contains(role):
@@ -70,8 +75,9 @@ class HasPermissionTrait:
     # def getAllPermissions(self,perms):
     #     return session.query(Permission).filter_by(Permission.slug.in_(perms)).all()
     
-    """check if user has direct permissions"""
     def hasPermission(perm):
+        """check if user has direct permissions"""
+        
         permit = Permission.query.filter_by(slug = perm).first()
         if permit is None:
             return False
