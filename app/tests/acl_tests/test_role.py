80fa74dba1bd4f45f1c837bdde773fc097fd2d44
loverofMush <tolujamal1@gmail.com>
2020-10-19 01:32:53 +0100

    - refactored create user role in tests - assert flash response if role already exists - test role can be updated - test role can be deleted

--- app/tests/acl_tests/test_role.py
+++ app/tests/acl_tests/test_role.py
@@ -3,39 +3,72 @@ from app.db import session
 from app.models.role import Role
 from app.tests.base import BaseCase
 from app.forms import *
+from app.controllers import SystemController
 
 class UserRoleTests(BaseCase):
+    def createUserRole(self):
+        role = Role('Cashier')
+        session.add(role)
+        session.commit()
+        
+        return role
+
+    
     def test_role_can_be_created(self):
         user = self.createUser()
         login = self.loginUser()
         
-        role = RoleForm(title = 'Cashier')
+        role_form = RoleForm(title = 'Cashier')
         
         with self.client:
             self.client.post(url_for('nonAuth.login'), data = login.data)
+            response = self.client.post(url_for('auth.createRole'), data = role_form.data)
             
-            response = self.client.post(url_for('auth.createRole'), data = role.data)
-            
+            roles = Role.query.all()
+            self.assertEqual(roles[0].title, 'Cashier')
             self.assertRedirects(response, url_for('auth.getRoles'))
             
+
     def test_role_already_exists(self):
         user = self.createUser()
         login = self.loginUser()
+        existing_role = self.createUserRole()
         
-        existing_role = Role(title = 'Cashier')
-        session.add(existing_role)
-        session.commit()
-        
-        role = RoleForm(title = 'Cashier')
+        role_form = RoleForm(title = 'Cashier')
         
         with self.client:
             self.client.post(url_for('nonAuth.login'), data = login.data)
-            response = self.client.post(url_for('auth.createRole'), data = role.data, follow_redirects=True)
+            response = self.client.post(url_for('auth.createRole'), data = role_form.data, follow_redirects=True)
             
-            try:
-                assert b'Role already exists!' in response.data
-            except AssertionError:
-                print("Assertion failed!")
+            assert b'Role already exists!' in response.data
+
             
-    # test_role_can_be_updated
-    # test_role_can_be_deleted
+    def test_role_can_be_updated(self):
+        user = self.createUser()
+        login = self.loginUser()
+        role = self.createUserRole()
+        
+        role_form = RoleForm(title = 'Cashier 1')
+        
+        with self.client:
+            self.client.post(url_for('nonAuth.login'), data = login.data)
+            response = self.client.post(url_for('auth.updateRole', id = 1), data = role_form.data)
+            
+            roles = Role.query.all()
+            self.assertEqual(roles[0].title, 'Cashier 1')
+            self.assertRedirects(response, url_for('auth.getRoles'))
+        
+        
+    def test_role_can_be_deleted(self):
+        user = self.createUser()
+        login = self.loginUser()
+        role = self.createUserRole()
+        
+        with self.client:
+            self.client.post(url_for('nonAuth.login'), data = login.data)
+            response = self.client.post(url_for('auth.deleteRole', id = 1))
+            
+            roles = Role.query.all()
+            
+            self.assertTrue(roles == [])
+            self.assertRedirects(response, url_for('auth.getRoles'))
