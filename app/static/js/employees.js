62fb74d9364bfc0b48b65619294ee3f1098d026d
loverofMush <tolujamal1@gmail.com>
2020-11-06 14:17:41 +0100

    - added local bootstrap - local jquery and popper - local fonts - local js files

---
+++ app/static/js/employees.js
@@ -0,0 +1,37 @@
+(function() {
+    searchEmployees = (e) => {
+        e.preventDefault();
+        var search_term = $('input[name=query]').val();
+
+        $.ajax(
+            {
+                type: "GET",
+                url: "{{url_for('auth.searchEmployees')}}",
+                data: { "query": search_term },
+                success: function (response) {
+                    $("#result").empty(); //remove whatever is there and append whatever is returned
+                    $('#result').empty().append("<option value='0'>Select</option>");
+                        response.results.forEach((value)=>{
+                            $('#result').append(
+                                `<option value="${value.id}">${value.first_name + ' ' + value.last_name}</option>`
+                            )
+
+                    });
+                }
+            }
+        )
+    }
+
+    selectEmployee = () => {
+        var sel = document.getElementById("result");
+        var id = sel.options[sel.selectedIndex].value;
+        var fullName = sel.options[sel.selectedIndex].text;
+
+        var names = fullName.split(" ");
+        var rev_names = names.reverse()
+
+        var username = rev_names.join(".").toLowerCase()
+        document.getElementById("username").value = username;
+        document.getElementById("employee").value = id;
+    }
+})();
