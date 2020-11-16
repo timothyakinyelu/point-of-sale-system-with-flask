fcbc4ae3209604f6173ccd932d0504c23876b3f0
loverofMush <tolujamal1@gmail.com>
2020-11-16 13:46:30 +0100

    - styled new user template

--- app/static/js/employees.js
+++ app/static/js/employees.js
@@ -10,13 +10,13 @@
                 url: "{{url_for('auth.searchEmployees')}}",
                 data: { "query": search_term },
                 success: function (response) {
-                    $("#result").empty(); //remove whatever is there and append whatever is returned
-                    $('#result').empty().append("<option value='0'>Select</option>");
-                        response.results.forEach((value)=>{
-                            $('#result').append(
-                                `<option value="${value.id}">${value.first_name + ' ' + value.last_name}</option>`
-                            )
-
+                    $("#employee").empty(); //remove whatever is there and append whatever is returned
+                    $('#employee').empty().append("<option value='0'>Select</option>");
+                    $('#employee').addClass('show')
+                    response.results.forEach((value)=>{
+                        $('#employee').append(
+                            `<option value="${value.id}">${value.first_name + ' ' + value.last_name}</option>`
+                        )
                     });
                 }
             }
@@ -25,7 +25,7 @@
 
     // select employee to be registered as a new user
     selectEmployee = () => {
-        var sel = document.getElementById("result");
+        var sel = document.getElementById("employee");
         var id = sel.options[sel.selectedIndex].value;
         var fullName = sel.options[sel.selectedIndex].text;
 
@@ -35,6 +35,9 @@
         var username = rev_names.join(".").toLowerCase()
         document.getElementById("username").value = username;
         document.getElementById("employee").value = id;
+
+        $("#employee").empty(); //remove whatever is there and append whatever is returned
+        $('#employee').removeClass('show')
     }
 
     $(document).ready(function() {
@@ -46,4 +49,10 @@
             }
         )
     });
+
+    // trigger submit button outside form
+    $('#saveUser').on('click', function(event) {
+        event.preventDefault();
+        $('#create_user').submit();
+    })
 })();
