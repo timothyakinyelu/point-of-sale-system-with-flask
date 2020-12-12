debe44cc37ad93412071f92b44aa1aaf7f26f61a
loverofMush <tolujamal1@gmail.com>
2020-12-12 12:43:32 +0100

    - import current_user - added flash message on success - changed create user template to dual functional template

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request, jsonify
+from flask_login import current_user
 from app.models import *
 from app.forms import *
 from app.db import session
@@ -32,6 +33,8 @@ def create():
             user.set_password(form.password.data)
             session.add(user)
             session.commit()
+            
+            flash('User created Successfully!')
             return redirect(url_for('auth.getUsers'))
         except AssertionError as exception_message:
             flash('{}'.format(exception_message))
@@ -39,8 +42,51 @@ def create():
     return render_template(
         'create_user.html', 
         form = form,
-        title = 'Create a New User.'
+        data_type = 'New User', 
+        form_action=url_for('auth.createUser'),
+        action = 'Add',
     )
+    
+def updateUser(user_id):
+    """ Update existing user in db"""
+   
+    user = User.query.filter_by(id = user_id).first()
+    form = CreateUserForm()
+    if request.method == 'GET':
+        form.username.data = user.username
+        form.role.data = user.role_id
+        form.employee.data = user.employee_id
+        form.shop.data = user.shop_id
+        form.status.data = user.status
+    
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                updatedUser = session.query(User).filter(User.id == user_id).update({
+                    User.username: form.username.data,
+                    User.status: form.status.data,
+                    User.role_id: form.role.data,
+                    User.employee_id: None if form.employee.data == '' else form.employee.data,
+                    User.shop_id: None if form.shop.data == 0 else form.shop.data
+                })
+                
+                updateUser.set_password(form.password.data)
+                session.commit()
+                
+                flash('User updated Successfully!')
+                return redirect(url_for('auth.getUsers'))
+            except AssertionError as exception_message:
+                flash('{}'.format(exception_message))
+        
+        flash('Unable to update user!')
+    return render_template(
+        'create_user.html',
+        form = form, 
+        data_type = user.username, 
+        form_action=url_for('auth.updateUser', user_id = user.id),
+        action = 'Edit',
+    )
+
     
 def searchEmployees():
     term = request.args.get('query')
